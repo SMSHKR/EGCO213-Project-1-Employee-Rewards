58760838ae0f8d0387f520cacc6ccd94ec790c31
SMSHKR <39004788+SMSHKR@users.noreply.github.com>
2018-09-29 20:44:28 +0700

    Add inputProduct(), TODO

--- src/Company.java
+++ src/Company.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Company {
@@ -14,8 +15,45 @@ public class Company {
         }
     }
 */
+    private static void inputProduct(ArrayList<Product> AL) {
+
+        Scanner kbScan = new Scanner(System.in);
+        String productFile = "test/products.txt";
+
+        boolean opened = false;
+        while (!opened)
+            try (Scanner infile = new Scanner(new File(productFile))) {
+                opened = true;
+
+                while (infile.hasNext()) {
+                    String line = infile.nextLine();
+                    String[] par = line.split(",");
+
+                    String name = par[0].trim();
+                    int price = Integer.parseInt(par[1].trim());
+
+                    AL.add(new Product(name, price));
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.out.print("Enter product file = ");
+                productFile = kbScan.next();
+            }
+
+    }
+
     public static void main(String[] args) {
         // test();
+
+        ArrayList<Product> productAL = new ArrayList<>();
+        inputProduct(productAL);
+        for (Product productArray : productAL) productArray.testPrint();
+
+        // TODO inputEmployee()
+
+        // TODO inputOvertime()
+
     }
-    
+
 }
--- src/Product.java
+++ src/Product.java
@@ -1,12 +1,17 @@
 public class Product {
-
+    // Variable
     private String name;
     private int price;
     private int totalSalesUnit;
     private int totalSalesBaht;
-
-    public Product() {
-
+    // Constructor
+    public Product(String inName, int inPrice) {
+        name = inName;
+        price = inPrice;
     }
+    // Method
+
+    // Debug Method
+    public void testPrint() { System.out.println( name + ", " + price ); }
 
 }
