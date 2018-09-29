bbae95a1a899d55e67a661add7a2d97efe65ae63
SMSHKR <39004788+SMSHKR@users.noreply.github.com>
2018-09-29 19:26:27 +0700

    Initialize Project

--- src/Company.java
+++ src/Company.java
@@ -1,6 +1,21 @@
-public class Company {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Company {
+/*
+    private static void test() {
+        try (Scanner file = new Scanner(new File("test/products.txt"))) {
+            while (file.hasNext())
+                System.out.println(file.nextLine());
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+*/
+    public static void main(String[] args) {
+        // test();
+    }
+    
 }
