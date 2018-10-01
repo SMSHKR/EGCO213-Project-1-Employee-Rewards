da71afe74630ba23096c45cc10524545ef84022d
rponly <43706421+rponly@users.noreply.github.com>
2018-10-01 21:56:33 +0700

    Update Company.java

--- src/Company.java
+++ src/Company.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Company {
 
     private static void test() {
-
+System.out.printf("Hello yeah");
     }
 
     private static void inputProduct(ArrayList<Product> AL) {
